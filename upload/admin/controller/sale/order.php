16c5055016cd027f3eda35b4d22c4bec880a186d
Oleg Kasyanov <oleg@batumi.org>
2026-03-09 01:50:57 +0400

    Fixed country & zone selector

--- upload/admin/controller/sale/order.php
+++ upload/admin/controller/sale/order.php
@@ -894,6 +894,14 @@ class Order extends \Opencart\System\Engine\Controller {
 			$data['payment_custom_field'] = [];
 		}
 
+		// Countries
+		$this->load->model('localisation/country');
+		$data['countries'] = $this->model_localisation_country->getCountries();
+
+		// Zones
+		$this->load->model('localisation/zone');
+		$data['payment_zones'] = $this->model_localisation_zone->getZonesByCountryId($data['payment_country_id']);
+
 		// Payment Method
 		if (!empty($order_info['payment_method'])) {
 			$data['payment_method_name'] = $order_info['payment_method']['name'];
